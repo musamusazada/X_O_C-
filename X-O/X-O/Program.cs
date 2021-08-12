773b0c2ff0c3cccf3df75b166a8e2649c30cb59b
Musa Musazada <musaam@code.edu.az>
2021-08-12 23:40:41 +0400

    Complete version

--- X-O/X-O/Program.cs
+++ X-O/X-O/Program.cs
@@ -6,22 +6,26 @@ namespace X_O
     {
         static void Main(string[] args)
         {
-            string[,] x_0 = new string[3, 3];
+            string[,] x_0 = new string[3, 4];
 
-            x_0[0, 0] = "_";
+            x_0[0, 0] = "1";
             x_0[0, 1] = "_";
             x_0[0, 2] = "_";
-            x_0[1, 0] = "_";
+            x_0[0, 3] = "_";
+            x_0[1, 0] = "2";
             x_0[1, 1] = "_";
             x_0[1, 2] = "_";
-            x_0[2, 0] = "_";
+            x_0[1, 3] = "_";
+            x_0[2, 0] = "3";
             x_0[2, 1] = "_";
             x_0[2, 2] = "_";
+            x_0[2, 3] = "_";
 
             char choice = ' ';
             int count = 1;
             do
             {
+                Console.WriteLine("   1 2 3");
                 DisplayArray(x_0);
                 Console.Write("Row: ");
                 int row = Convert.ToInt32(Console.ReadLine());
@@ -29,11 +33,11 @@ namespace X_O
                 int column = Convert.ToInt32(Console.ReadLine());
                 if (count % 2 != 0)
                 {
-                    x_0[row, column] = "X";
+                    x_0[row-1, column] = "X";
                 }
                 else
                 {
-                    x_0[row, column] = "O";
+                    x_0[row-1, column] = "O";
                 }
 
                 DisplayArray(x_0);
@@ -75,7 +79,7 @@ namespace X_O
         }
         static bool CheckWin(string[,] x_0, ref int count)
         {
-            if ((x_0[0, 0] == "X" && x_0[0, 1] == "X" && x_0[0, 2] == "X") || (x_0[0, 0] == "O" && x_0[0, 1] == "O" && x_0[0, 2] == "O"))
+            if ((x_0[0, 1] == "X" && x_0[0, 2] == "X" && x_0[0, 3] == "X") || (x_0[0, 1] == "O" && x_0[0, 2] == "O" && x_0[0, 3] == "O"))
             {
                 if (count % 2 != 0)
                 {
@@ -92,7 +96,7 @@ namespace X_O
 
 
             }
-            else if ((x_0[1, 0] == "X" && x_0[1, 1] == "X" && x_0[1, 2] == "X") || (x_0[1, 0] == "O" && x_0[1, 1] == "O" && x_0[1, 2] == "O"))
+            else if ((x_0[1, 1] == "X" && x_0[1, 2] == "X" && x_0[1, 3] == "X") || (x_0[1, 1] == "O" && x_0[1, 2] == "O" && x_0[1, 3] == "O"))
             {
                 if (count % 2 != 0)
                 {
@@ -107,7 +111,7 @@ namespace X_O
 
 
             }
-            else if ((x_0[2, 0] == "X" && x_0[2, 1] == "X" && x_0[2, 2] == "X") || (x_0[2, 0] == "O" && x_0[2, 1] == "O" && x_0[2, 2] == "O"))
+            else if ((x_0[2, 1] == "X" && x_0[2, 2] == "X" && x_0[2, 3] == "X") || (x_0[2, 1] == "O" && x_0[2, 2] == "O" && x_0[2, 3] == "O"))
             {
                 if (count % 2 != 0)
                 {
@@ -122,7 +126,7 @@ namespace X_O
 
 
             }
-            else if ((x_0[0, 0] == "X" && x_0[1, 1] == "X" && x_0[2, 2] == "X") || (x_0[0, 0] == "O" && x_0[1, 1] == "O" && x_0[2, 2] == "O"))
+            else if ((x_0[0, 1] == "X" && x_0[1, 2] == "X" && x_0[2, 3] == "X") || (x_0[0, 1] == "O" && x_0[1, 2] == "O" && x_0[2, 3] == "O"))
             {
                 if (count % 2 != 0)
                 {
@@ -137,22 +141,7 @@ namespace X_O
 
 
             }
-            else if ((x_0[0, 2] == "X" && x_0[1, 1] == "X" && x_0[2, 0] == "X") || (x_0[0, 2] == "O" && x_0[1, 1] == "O" && x_0[2, 0] == "O"))
-            {
-                if (count % 2 != 0)
-                {
-                    Console.WriteLine("O WON!!!");
-                    return true;
-                }
-                else
-                {
-                    Console.WriteLine("X WON!!!");
-                    return true;
-                }
-
-
-            }
-            else if ((x_0[0, 0] == "X" && x_0[1, 0] == "X" && x_0[2, 0] == "X") || (x_0[0, 0] == "O" && x_0[1, 0] == "O" && x_0[2, 0] == "O"))
+            else if ((x_0[0, 3] == "X" && x_0[1, 2] == "X" && x_0[2, 1] == "X") || (x_0[0, 3] == "O" && x_0[1, 2] == "O" && x_0[2, 1] == "O"))
             {
                 if (count % 2 != 0)
                 {
@@ -194,13 +183,28 @@ namespace X_O
                     Console.WriteLine("X WON!!!");
                     return true;
                 }
+
+
+            }
+            else if ((x_0[0, 3] == "X" && x_0[1, 3] == "X" && x_0[2, 3] == "X") || (x_0[0, 3] == "O" && x_0[1, 3] == "O" && x_0[2, 3] == "O"))
+            {
+                if (count % 2 != 0)
+                {
+                    Console.WriteLine("O WON!!!");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("X WON!!!");
+                    return true;
+                }
             }
             else
             {
                 return false;
             }
 
-
+            
         }
     }
 }
